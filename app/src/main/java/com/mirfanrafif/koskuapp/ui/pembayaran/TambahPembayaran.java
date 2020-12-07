670c5bf801f27bf96fe2f8c744fcd9724433c0f3
Irfan Rafif <mirfanrafif17@gmail.com>
2020-12-07 07:50:24 +0700

    hapus data binding di tambah pembayaran

--- app/src/main/java/com/mirfanrafif/koskuapp/ui/pembayaran/TambahPembayaran.java
+++ app/src/main/java/com/mirfanrafif/koskuapp/ui/pembayaran/TambahPembayaran.java
@@ -3,16 +3,13 @@ package com.mirfanrafif.koskuapp.ui.pembayaran;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.TextView;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import com.mirfanrafif.koskuapp.MainActivity;
 import com.mirfanrafif.koskuapp.R;
-import com.mirfanrafif.koskuapp.databinding.ActivityTambahPembayaranBinding;
 import com.mirfanrafif.koskuapp.models.AnakKos;
 import com.mirfanrafif.koskuapp.models.Pembayaran;
 import com.mirfanrafif.koskuapp.services.AnakKosApi;
@@ -27,22 +24,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TambahPembayaran extends AppCompatActivity {
-    ActivityTambahPembayaranBinding binding;
+
+    Spinner namaAnakKos;
+    EditText bulanEdit, tahunEdit;
+    Button simpanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView
-                (this, R.layout.activity_tambah_pembayaran);
+        setContentView(R.layout.activity_tambah_pembayaran);
 
-        TambahPembayaranViewModel viewModel = new TambahPembayaranViewModel();
-        binding.setViewModel(viewModel);
+        namaAnakKos = findViewById(R.id.namaAnakKosSpinner);
+        bulanEdit = findViewById(R.id.bulanInput);
+        tahunEdit = findViewById(R.id.tahunInput);
+        simpanButton = findViewById(R.id.simpanButton);
 
-        binding.getViewModel().getAnakKos().observe(this, new Observer<List<AnakKos>>() {
+        new AnakKos().getListAnakKos().observe(this, new Observer<List<AnakKos>>() {
             @Override
             public void onChanged(List<AnakKos> anakKos) {
                 ArrayList<String> listNama = new ArrayList<>();
-                for (AnakKos item: binding.getViewModel().getAnakKos().getValue()) {
+                for (AnakKos item: anakKos) {
                     listNama.add(item.getNama() + " - " + item.getId());
                 }
 
@@ -51,11 +52,13 @@ public class TambahPembayaran extends AppCompatActivity {
                         R.layout.support_simple_spinner_dropdown_item,
                         listNama
                 );
-                binding.namaAnakKosSpinner.setAdapter(adapter);
+
+                namaAnakKos.setAdapter(adapter);
             }
+
         });
 
-        binding.simpanButton.setOnClickListener(new View.OnClickListener() {
+        simpanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 saveData();
@@ -67,14 +70,14 @@ public class TambahPembayaran extends AppCompatActivity {
 
     private void saveData() {
         Pembayaran pembayaran = new Pembayaran();
-        String id = binding.namaAnakKosSpinner.getSelectedItem().toString().split(" - ")[1];
+        String id = namaAnakKos.getSelectedItem().toString().split(" - ")[1];
 //        Log.d("error", id);
         new AnakKos().getAnakKosById(id).observe(this, new Observer<AnakKos>() {
             @Override
             public void onChanged(AnakKos anakKos) {
                 pembayaran.setIdanakkos(anakKos);
-                pembayaran.setBulan(Integer.parseInt(binding.bulanInput.getText().toString()));
-                pembayaran.setTahun(Integer.parseInt(binding.tahunInput.getText().toString()));
+                pembayaran.setBulan(Integer.parseInt(bulanEdit.getText().toString()));
+                pembayaran.setTahun(Integer.parseInt(tahunEdit.getText().toString()));
 
                 pembayaran.saveAnakKos();
             }
